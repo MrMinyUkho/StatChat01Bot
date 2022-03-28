8d772d439410a00641b4cce4c3699ef2693b4a7f
MrMinyUkho <ADimaso007@gmail.com>
2022-03-28 20:33:58 +0300

    Add init bash file

---
+++ init.sh
@@ -0,0 +1,11 @@
+mkdir config
+touch config/config.py
+
+echo "TOKEN=\"Your token\"
+RANDOM_KEY=\"Your api key to random.org\"" >> ./config/config.py
+
+mkdir AudioFile
+mkdir Cert
+mkdir Photo
+mkdir PhotoOut
+mkdir StatPlot
